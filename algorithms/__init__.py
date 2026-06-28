a3da4d4d99105f1ef7e67864401d96482e6b2995
tzy <503761171@qq.com>
2026-06-28 16:35:46 +0800

    add Selective-SEAC: importance ratio × |advantage| filtering

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -3,3 +3,4 @@ from algorithms.iac import IAC
 from algorithms.snac import SNAC
 from algorithms.seac import SEAC
 from algorithms.seac_pooled import SEACPooled
+from algorithms.seac_selective import SEACSelective
